f53e5153f1533b540ed6020c1f8f6d3ece5da923
KnightsWhoSayNi0 <62893792+KnightsWhoSayNi0@users.noreply.github.com>
2021-07-19 00:07:44 -0400

    add splash screen

---
+++ src/gfx/arrl.h
@@ -0,0 +1,18 @@
+#ifndef arrl_include_file
+#define arrl_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define arrl_width 50
+#define arrl_height 114
+#define arrl_size 5702
+#define arrl ((gfx_sprite_t*)arrl_data)
+extern unsigned char arrl_data[5702];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  *--------------------------------------
  * Program Name: Ham Radio Utilities CE
  * Author: KnightsWhoSayNi
- * License: MIT
+ * License:
  * Description: ARRL Ham Radio Utilities for the CE
  *--------------------------------------
 */
@@ -12,15 +12,26 @@
 #include <graphx.h>
 #include <keypadc.h>
 
+#include "gfx/gfx.h"
+
 int main(void)
 {
+
     os_ClrHome();
     gfx_Begin();
-    gfx_FillScreen(255);
+    gfx_SetPalette(global_palette, sizeof_global_palette, 0);
 
-    while (!os_GetCSC()) {
-        
-    }
+    gfx_FillScreen(1);
+    gfx_Sprite(arrl, 25, 60);
+    gfx_SetTextScale(2, 2);
+    gfx_PrintStringXY("Ham Radio", 130, 60);
+    gfx_PrintStringXY("Utilities CE", 120, 80);
+    gfx_SetTextScale(1, 1);
+    gfx_PrintStringXY("By KnightsWhoSayNi", 130, 160);
+
+    delay(1000);
+
+    
 
     gfx_End();
     return 0;
